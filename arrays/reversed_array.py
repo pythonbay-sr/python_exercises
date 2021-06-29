761fbe330a745cf9604866ed5f6c412f326ac266
Nikola Kostic <60295948+nikolakosticc@users.noreply.github.com>
2021-06-29 19:01:30 +0200

    Update reversed_array.py

--- arrays/reversed_array.py
+++ arrays/reversed_array.py
@@ -1,9 +1,6 @@
 array = [1, 3, 5, 3, 7, 1, 9, 3]
 reversed_array = []
 
-length = len(array)
-
-
 for i in range(len(array)):
                num = array[-1] #take a last number from the array
                del array[-1] #delete it
